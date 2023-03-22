56799c3c4d4236314862fa150e12ca25ff5547d1
hefl5296 <hefl5296@colorado.edu>
2023-03-21 20:16:17 -0600

    testing out api use with data

--- DataExp.py
+++ DataExp.py
@@ -1,2 +1,9 @@
 import requests
+from octanegg import Octane
+import pandas as pd
 
+response = requests.get("https://zsr.octane.gg/players?tag=noly")
+response = response.json()
+user = pd.read_json(response)
+print(user.to_string())
+# print(response.json())
