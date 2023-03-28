a9373db65b23fd06bd82e57ed81a031a8591e812
fifo9756 <59852871+fifo9756@users.noreply.github.com>
2023-03-28 17:54:28 -0600

    Add files via upload

---
+++ frontend/tabs.js
@@ -0,0 +1,16 @@
+function openTab(tabName) {
+  var i, tabcontent, tablinks;
+  // bone 
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
