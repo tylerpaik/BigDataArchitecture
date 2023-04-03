7991b0cdeac472fa738a3a87f7fa3a7f27c6b056
hefl5296 <hefl5296@colorado.edu>
2023-04-03 14:56:30 -0600

    got user id from pandas df with api response

--- DataExp.py
+++ DataExp.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 response = requests.get("https://zsr.octane.gg/players?tag=noly")
 response = response.json()
-user = pd.read_json(response)
-print(user.to_string())
-# print(response.json())
+keys = response.keys()
+user = pd.DataFrame(response['players'])
+print(user)
+u_id = user.iloc[0]['_id']
+print(u_id)
