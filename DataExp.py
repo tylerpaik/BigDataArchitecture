87e35dc820b6560764154698072f3af373b12351
hefl5296 <hefl5296@colorado.edu>
2023-04-03 21:30:42 -0600

    api no worky:(

--- DataExp.py
+++ DataExp.py
@@ -9,3 +9,9 @@ user = pd.DataFrame(response['players'])
 print(user)
 u_id = user.iloc[0]['_id']
 print(u_id)
+
+stats_api = " https://zsr.octane.gg/stats/players" #?player=5f3d8fdd95f40596eae240d3
+# stats_api += "player="+ str(u_id)
+print(stats_api)
+stats = requests.get(stats_api)
+print(stats.json)
