392a08ca0fc28f9a60a0be2c2aa84f96b9313d21
hefl5296 <hefl5296@colorado.edu>
2023-04-06 15:16:47 -0600

    more stuff w api

--- DataExp.py
+++ DataExp.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import numpy as np
 import json
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -36,18 +37,35 @@ currEventStats_df = pd.DataFrame(eventStats['stats'])
 gas = currEventStats_df.iloc[0]['stats']
 gas_df = pd.DataFrame(gas.items())
 gas_df.rename(columns={0:'Stat Type', 1: 'Stats'}, inplace = True)
+# gas_df.sort_values('startDate')
 sns.set()
 sns.barplot(data=gas_df, x='Stat Type', y ='Stats')
 plt.show()
 # currEvent_saves = currEventStats_df.iloc['stats']
 # saves.append(currEvent_saves)
 # print(saves)
+gas_df = pd.DataFrame()
+dates = []
+for i in range(10):
+    e_id = event_df.iloc[i]['_id']
+    eventStats_url = "https://zsr.octane.gg/stats/players/events?stat=goals&stat=assists&stat=saves&event=" + str(e_id) + "&player=" + str(u_id)
+    eventStats = requests.get(eventStats_url)
+    eventStats = eventStats.json()
+    currEventStats_df = pd.DataFrame(eventStats['stats'])
+    currEventStats_df.sort_values(by="startDate", axis=0, inplace=True)
+    gas = currEventStats_df.iloc[0]['stats']
+    gas_df = pd.DataFrame(gas.items())
+    gas_df["startDate"] = currEventStats_df.iloc[0]["startDate"]
+    # gas_df.rename(columns={0:'Stat Type', 1: 'Stats'}, inplace = True)
+    print(gas_df)
+    saves.append(gas_df.iloc[2][1])
+    goals.append(gas_df.iloc[1][1])
+    assists.append(gas_df.iloc[0][1])
+    dates.append(gas_df.iloc[0]["startDate"]) 
+saves = np.array(saves)
+goals = np.array(goals)
+assists = np.array(assists)
+# saves_avg = np.average(saves)
+# goals_avg = np.average(goals)
+# assists_avg = np.average(assists)
 
-
-# for i in range(10):
-#     e_id = event_df.iloc[i]['_id']
-#     eventStats_url = "https://zsr.octane.gg/stats/players/events?stat=goals&stat=assists&stat=saves&event=" + str(e_id)
-#     eventStats = requests.get(eventStats_url)
-#     eventStats = eventStats.json()
-#     currEventStats_df = pd.DataFrame(eve)
-#     saves.append(eventStats_url
