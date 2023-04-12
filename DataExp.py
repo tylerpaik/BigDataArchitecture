ccd08930846f72cf8a487136aa64566d9ea361e3
hefl5296 <hefl5296@colorado.edu>
2023-04-11 18:03:06 -0600

    made plots and formatted data for individual timeline plots

--- DataExp.py
+++ DataExp.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 import seaborn as sns
 import matplotlib.pyplot as plt
+from datetime import datetime, timezone
 
 response = requests.get("https://zsr.octane.gg/players?tag=noly")
 response = response.json()
@@ -24,7 +25,7 @@ event_stat = user_stats.iloc[0]['events']
 event_df = pd.DataFrame(event_stat)
 print(event_df)
 # print(user_stats)
-event_id10 = []
+# event_id10 = []
 saves = []
 assists = []
 goals = []
@@ -38,9 +39,9 @@ gas = currEventStats_df.iloc[0]['stats']
 gas_df = pd.DataFrame(gas.items())
 gas_df.rename(columns={0:'Stat Type', 1: 'Stats'}, inplace = True)
 # gas_df.sort_values('startDate')
-sns.set()
-ax = sns.barplot(data=gas_df, x='Stat Type', y ='Stats')
-plt.show()
+# sns.set()
+# ax = sns.barplot(data=gas_df, x='Stat Type', y ='Stats')
+# plt.show()
 # currEvent_saves = currEventStats_df.iloc['stats']
 # saves.append(currEvent_saves)
 # print(saves)
@@ -55,16 +56,34 @@ for i in range(10):
     currEventStats_df.sort_values(by="startDate", axis=0, inplace=True)
     gas = currEventStats_df.iloc[0]['stats']
     gas_df = pd.DataFrame(gas.items())
-    gas_df["startDate"] = currEventStats_df.iloc[0]["startDate"]
+    gas_df["startDate"] = currEventStats_df.iloc[0]["startDate"] #adding dates for visualization 
     # gas_df.rename(columns={0:'Stat Type', 1: 'Stats'}, inplace = True)
-    print(gas_df)
+    # print(gas_df)
     saves.append(gas_df.iloc[2][1])
     goals.append(gas_df.iloc[1][1])
     assists.append(gas_df.iloc[0][1])
-    dates.append(gas_df.iloc[0]["startDate"]) 
-saves = np.array(saves)
-goals = np.array(goals)
-assists = np.array(assists)
+    dates.append(gas_df.iloc[0]["startDate"])
+for j in range(len(dates)):
+    dates[j] = dates[j][:10] #get only date from timestamp string (makes it more readable)
+sorting = sorted(zip(dates, saves, goals, assists)) #sorting parallel lists
+sorted = list(zip(*sorting))
+dates = list(sorted[0])
+saves = list(sorted[1])
+goals = list(sorted[2])
+assists = list(sorted[3])
+saves_df = pd.DataFrame({'Saves': saves, 'Time': dates})
+goals_df = pd.DataFrame({'Goals': goals, 'Time': dates})
+assists_df = pd.DataFrame({'Assists': assists, 'Time': dates})
+sns.set()
+ax = sns.lineplot(data=saves_df, x='Time', y ='Saves')
+ax2 = sns.lineplot(data=goals_df, x ='Time', y = 'Goals')
+ax3 = sns.lineplot(data=assists_df, x = 'Time', y = 'Assists')
+plt.show()
+
+
+# saves = np.array(saves)
+# goals = np.array(goals)
+# assists = np.array(assists)
 # saves_avg = np.average(saves)
 # goals_avg = np.average(goals)
 # assists_avg = np.average(assists)
