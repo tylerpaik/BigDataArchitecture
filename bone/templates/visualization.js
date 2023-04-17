e4aac7d424f0df576fdc0598c04b1a90424f119b
hefl5296 <hefl5296@colorado.edu>
2023-04-17 15:45:34 -0600

    moving html/css/js files

---
+++ bone/templates/visualization.js
@@ -0,0 +1,5 @@
+d3.json( "source", function(error, data) {
+  if (error) throw error;
+
+  // vis code dep. on data format etc.
+});
--- frontend/visualization.js
+++
@@ -1,5 +0,0 @@
-d3.json( "source", function(error, data) {
-  if (error) throw error;
-
-  // vis code dep. on data format etc.
-});
