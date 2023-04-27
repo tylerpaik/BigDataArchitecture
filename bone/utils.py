3dd104e42c5c24757a4ff71267049a39ed1381fe
RyanSimcoxCSCI <99707726+RyanSimcoxCSCI@users.noreply.github.com>
2023-04-27 03:43:41 -0600

    Modified calc_probability, added Redis support
    Takes in a player id and prediction to calculate probability of player meeting prediction

--- bone/utils.py
+++ bone/utils.py
@@ -3,7 +3,9 @@ from flask import Flask, jsonify
 import pandas as pd
 import schedule
 import time
-# from scipy import poisson
+from data import retrieve_player_dfs
+import numpy as np
+from scipy import poisson
 
 from config import redis_client
 
@@ -28,18 +30,27 @@ def scheduler_loop():
 #     unique_team_names = pd.Series(get_team_names())
 #     return jsonify(team_names=unique_team_names.tolist())
 
-# def calc_probability(n_gas: int, df: pd.DataFrame) -> float:
-#     if n_gas < 0:
-#         raise ValueError("The number of goals/assists/scores can't be negative.")
-#     # stats are always in the first column, extract as a list
-#     data = df.iloc[:, 0].tolist()
-#     # calculate the mean nr of goals (lambda)
-#     mean_gas = sum(data) / len(data)
-#     # def Poisson(lambda = mean_gas)
-#     poisson_dist = poisson(mean_gas)
-#     # store discrete pmf
-#     n_gas_prob = poisson_dist.pmf(n_gas)
-#     print("The probability of scoring ", n_gas, " units is ", n_gas_prob)
-#     return n_gas_prob
+#Pred is for number of goals user wants probability for
+def calc_probability(player_id: str, pred: int) -> float:
+    goals_df, saves_df, assists_df, averages_df = retrieve_player_dfs(player_id)
+    num_goals = np.sum(goals_df)
+
+    #can add functionality for saves/assists
+    num_saves = np.sum(saves_df)
+    num_assists = np.sum(assists_df)
+
+    # if n_gas < 0:
+    #     raise ValueError("The number of goals/assists/scores can't be negative.")
+    # # stats are always in the first column, extract as a list
+    # data = df.iloc[:, 0].tolist()
+
+    # calculate the mean nr of goals (lambda)
+    mean_goals = num_goals / goals_df.shape[0]
+    # def Poisson(lambda = mean_gas)
+    poisson_dist = poisson(mean_goals)
+    # store discrete pmf
+    num_goals_prob = poisson_dist.pmf(pred))
+    print("The probability of scoring ", pred, " goals is ", num_goals_prob)
+    return num_goals_prob
 # probability fc end
 
